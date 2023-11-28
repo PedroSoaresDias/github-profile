e530fae5d67c6857459243ab0089e9de5b332be1
Pedro Soares Dias <pedroferraridias@gmail.com>
2023-11-28 12:19:09 -0300

    Centralizando os dados do usuário do GitHub

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -2,31 +2,35 @@ import { getUserGitHub } from "@/app/lib/getUserGitHub";
 import Image from "next/image";
 import Link from "next/link";
 
-export default async function Usuario({ params: { username }, }: { params: { username: string } }) {
+export default async function Usuario({ params: { username } }: { params: { username: string } }) {
 
     try {
         const developer = await getUserGitHub(username);
 
-        if(!developer) return <p>Usuário não encontrado</p>
+        if (!developer) return <p>Usuário não encontrado</p>
 
         const createdAt = new Date(developer.created_at).toLocaleDateString("pt-BR");
         const updadedAt = new Date(developer.updated_at).toLocaleDateString("pt-BR");
 
         return (
-            <section className="py-3 px-4 bg-black min-h-screen text-gray-50" key={developer.id}>
-                <Link href={"/"} className="my-10 ml-5 font-semibold text-lg">Voltar para a página principal</Link>
+            <section className="py-2 flex flex-col justify-center items-center bg-black min-h-screen text-gray-50" key={developer.id}>
+                <Link href={"/"} className="my-3 text-center justify-start font-semibold text-lg bg-purple-700 rounded-lg px-6 py-2 transition-all duration-300 hover:bg-purple-800 ">Retornar para pesquisa</Link>
                 <br />
-                <Image className="rounded-3xl mt-6" src={developer.avatar_url} alt={developer.login} width={300} height={300} />
-                <br />
-                <h3>Nome de usuário: {developer.login}</h3>
-                <br />
-                <p>Tipo da conta: {developer.type ? developer.type : "Desconhecido"}</p>
-                <p>Data de criação: {createdAt}</p>
-                <p>Última atualização: {updadedAt}</p>
-                <p>Sua descrição: {developer.bio ? developer.bio : "Não Informado"}</p>
-                <p>Repositórios públicos: {developer.public_repos}</p>
-                <p>Seguidores: {developer.followers}</p>
-                <p>Seguindo: {developer.following}</p>
+                <div className="flex flex-col  justify-center items-center w-11/12 md:w-9/12 sm:w-1/2">
+                    <Image className="rounded-3xl mt-3" src={developer.avatar_url} alt={developer.login} width={300} height={300} />
+                    <br />
+                    <h3>{developer.login}</h3>
+                    <br />
+                    <div className="justify-start gap-1 p-1 m-1">
+                        <p>Tipo da conta: {developer.type ? developer.type : "Desconhecido"}</p>
+                        <p>Data de criação: {createdAt}</p>
+                        <p>Última atualização: {updadedAt}</p>
+                        <p>Sua descrição: {developer.bio ? developer.bio : "Não Informado"}</p>
+                        <p>Repositórios públicos: {developer.public_repos}</p>
+                        <p>Seguidores: {developer.followers}</p>
+                        <p>Seguindo: {developer.following}</p>
+                    </div>
+                </div>
             </section>
         );
     } catch (error) {
--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -4,7 +4,7 @@ export function GetUser({ username }: { username: string }) {
     return (
         <Link
             href={`/user/${username}`}
-            className="bg-purple-700 text-center text-white rounded-lg px-4 py-2 font-semibold hover:bg-purple-800 transition-all duration-200"
+            className="bg-purple-700 text-center text-white rounded-lg px-4 py-2 font-semibold hover:bg-purple-800 transition-all duration-300"
         >
             Pesquisar
         </Link>
