b705841b16c3fbca3b0a2af701865ef1b3b2e5e1
Pedro Soares Dias <pedroferraridias@gmail.com>
2024-01-23 19:08:38 -0300

    Atualizando as dependencias e fazendo a revalidação da API

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -10,7 +10,7 @@ function handleAPI(response: Response) {
 
 export async function getUserGitHub(username: string) {
   try {
-    const user = await fetch(`https://api.github.com/users/${username}`, { cache: 'no-store' });
+    const user = await fetch(`https://api.github.com/users/${username}`, { next: { revalidate: 10 } });
 
     return handleAPI(user);
   } catch (error) {
@@ -21,7 +21,7 @@ export async function getUserGitHub(username: string) {
 
 export async function getSearchUserGitHub(username: string) {
   try {
-    const search = await fetch(`https://api.github.com/search/users?q=${username}`, { cache: 'no-store' });
+    const search = await fetch(`https://api.github.com/search/users?q=${username}`, { next: { revalidate: 10 } });
 
     return handleAPI(search);
   } catch (error) {
@@ -36,7 +36,7 @@ function calculateTotalPagesWithModulo(totalItems: number, itemsByPage: number):
 
 export async function getRepositoriesGitHub(username: string) {
   try {
-    const repos = await fetch(`https://api.github.com/users/${username}/repos`, { cache: 'no-store'});
+    const repos = await fetch(`https://api.github.com/users/${username}/repos`, { next: { revalidate: 10 } });
 
     if (!repos.ok) {
       const errorResponse = await repos.json();
@@ -58,7 +58,7 @@ export async function getRepositoriesByPage(username: string, currentPage: numbe
   const page = currentPage;
   
   try {
-    const repos = await fetch(`https://api.github.com/users/${username}/repos?page=${page}&per_page=${perPage}`, { cache: 'no-store'});
+    const repos = await fetch(`https://api.github.com/users/${username}/repos?page=${page}&per_page=${perPage}`, { next: { revalidate: 10 } });
 
     return handleAPI(repos);
   } catch (error) {
