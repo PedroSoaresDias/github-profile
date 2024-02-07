cd6469abb9c62356510042ec606fdfcbe4e192aa
Pedro Soares Dias <pedroferraridias@gmail.com>
2024-02-07 17:47:41 -0300

    Ajustando o código do loading.tsx

--- src/app/user/[username]/loading.tsx
+++ src/app/user/[username]/loading.tsx
@@ -1,10 +1,10 @@
 import { FaReact } from 'react-icons/fa6'
 
 export default function Loading() {
-    return (
-        <div className="bg-black min-h-screen flex justify-center items-center flex-col text-white font-semibold">
-            <FaReact className='text-blue-500 animate-spin' size={100} />
-            Carregando...
-        </div>
-    )
+  return (
+    <div className="bg-black min-h-screen flex justify-center items-center flex-col text-white font-semibold">
+      <FaReact className='text-blue-500 animate-spin' size={100} />
+      Carregando...
+    </div>
+  )
 }
