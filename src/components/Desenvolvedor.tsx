336afaa67cc362508e955d5e6b4c93dcaa8c247c
Pedro Soares Dias <pedroferraridias@gmail.com>
2024-06-11 13:14:13 -0300

    Implementando o DaisyUI no projeto

--- src/components/Desenvolvedor.tsx
+++ src/components/Desenvolvedor.tsx
@@ -10,7 +10,7 @@ export async function Desenvolvedor({ username }: { username: string }) {
 
   return (
     <section className="py-2 flex flex-col justify-center items-center dark:bg-black bg-gray-100 min-h-screen text-gray-950 dark:text-gray-100" key={developer.id}>
-      <Link href={"/"} className="my-3 text-center text-gray-100 justify-start font-semibold text-lg bg-purple-700 rounded-lg px-6 py-2 transition-all duration-300 hover:bg-purple-900">Retornar para pesquisa</Link>
+      <Link href={"/"} className="btn my-3 text-center text-gray-100 justify-start font-semibold text-lg bg-purple-700 rounded-lg px-6 py-2 transition-all duration-300 hover:bg-purple-900">Retornar para pesquisa</Link>
       <br />
       <div className="flex flex-col  justify-center items-center w-11/12 md:w-9/12 sm:w-1/2">
         <Image unoptimized className="rounded-3xl mt-3" src={developer.avatar_url} alt={`foto do usuário ${developer.login}`} width={300} height={300} quality={75} />
@@ -29,7 +29,7 @@ export async function Desenvolvedor({ username }: { username: string }) {
       </div>
       <a
         href={developer.html_url}
-        className="text-center bg-purple-700 text-gray-50 font-semibold hover:bg-purple-900 py-2 px-5 my-3 rounded-lg text-lg duration-300 mb-3 transition-all"
+        className="btn text-center bg-purple-700 text-gray-50 font-semibold hover:bg-purple-900 py-2 px-5 my-3 rounded-lg text-lg duration-300 mb-3 transition-all"
         target="_blank"
       >
         Saiba mais
--- src/components/Pesquisar.tsx
+++ src/components/Pesquisar.tsx
@@ -24,7 +24,7 @@ export default function Pesquisar({ placeholder }: { placeholder: string }) {
     <div className='flex flex-col'>
       <label htmlFor="username" className='dark:text-gray-50 text-gray-950 text-sm md:text-base sm:text-lg font-bold'>Digita o nome de usuário do GitHub</label>
       <input
-        className='my-3 p-1 dark:bg-gray-950 bg-gray-50 shadow border-2 border-gray-800 dark:border-gray-300 shadow-gray-900 dark:shadow-gray-100 text-gray-950 dark:text-gray-50 rounded-md text-base dark:placeholder:text-gray-400 placeholder:text-gray-800'
+        className='my-3 p-2 dark:bg-gray-950 bg-gray-50 shadow border-2 border-gray-800 dark:border-gray-300 shadow-gray-900 dark:shadow-gray-100 text-gray-950 dark:text-gray-50 rounded-lg text-base dark:placeholder:text-gray-400 placeholder:text-gray-800'
         placeholder={placeholder}
         onChange={(e) => { handleSearch(e.target.value) }}
         defaultValue={searchParams.get('query')?.toString()}
