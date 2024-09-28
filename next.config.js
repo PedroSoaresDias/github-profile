e59e7471786e33f3a8b88a962e89d4e5fb30725e
Pedro Soares Dias <pedroferraridias@gmail.com>
2024-09-28 15:53:22 -0300

    Comentando o CSP

--- next.config.js
+++ next.config.js
@@ -27,18 +27,18 @@ module.exports = withPWA({
   swcMinify: true
 });
 
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self';"
-          },
-        ],
-      },
-    ];
-  },
-};
+// module.exports = {
+//   async headers() {
+//     return [
+//       {
+//         source: '/(.*)',
+//         headers: [
+//           {
+//             key: 'Content-Security-Policy',
+//             value: "default-src 'self'; script-src 'self';"
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
