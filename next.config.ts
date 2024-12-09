d827948cad807a4450c564b7efc302437cb97f09
Pedro Soares Dias <pedroferraridias@gmail.com>
2024-12-09 19:50:39 -0300

    Adicionando novamente o React Compiler

--- next.config.ts
+++ next.config.ts
@@ -9,9 +9,9 @@ const nextConfig: NextConfig = {
       }
     ]
   },
-  // experimental: {
-  //   reactCompiler: true
-  // }
+  experimental: {
+    reactCompiler: true
+  }
 }
 
 export default nextConfig;
