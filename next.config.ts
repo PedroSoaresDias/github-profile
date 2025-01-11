ad211d7118797530925eaa146feb67e2dd399d2a
Pedro Soares Dias <pedroferraridias@gmail.com>
2025-01-11 20:47:37 -0300

    Atualizando as dependências e ajustando o next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,9 +9,9 @@ const nextConfig: NextConfig = {
       }
     ]
   },
-  experimental: {
-    reactCompiler: true
-  }
+  // experimental: {
+  //   reactCompiler: false
+  // }
 }
 
 export default nextConfig;
